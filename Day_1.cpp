4c126d3be6dcea6a626783e09e38d361f8f7e96e
Farhan Sadeek <farhansadeek019@gmail.com>
2022-12-01 22:25:40 -0500

    Update

--- Day_1.cpp
+++ Day_1.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main(){
     ofstream file;
     file.open("Day_1.txt");
-    for(int i = 0; i < 2261; i++){
-        // cout << ;
-    }
+    
     file.close();
     return 0;
 }
