aa3a1cdd3159fda3e43d33f69f37ada9d2ee3b3f
SadeekFarhan21 <farhansadeek019@gmail.com>
2022-12-06 21:30:04 -0500

    Day 4 Advent of Code

---
+++ Day_4.py
@@ -0,0 +1,20 @@
+with open('Day_4.in') as file:
+    string_list = [i for i in file.read().strip().split('\n')]
+count = 0
+for i in string_list:
+        List = i.split(',')
+        interval = List[0].strip().split('-') # Where to find
+        given = List[1].strip().split('-')  # What to find
+        lower_limit = int(interval[0]) # Lower limit of where to find
+        upper_limit = int(interval[1]) # Upper limit of where to find
+        given_lower = int(given[0]) # Lower limit of what to find
+        given_higher = int(given[1]) # Upper limit of what to find
+        '''
+        print(interval, given) # Printing out the list
+        print(lower_limit, upper_limit)
+        print(given_lower, given_higher)
+        '''
+        if(lower_limit <= given_lower and upper_limit >= given_higher):
+            print(lower_limit, upper_limit, given_lower, given_higher)
+            count += 1    
+print(count)
